d838cd28ea2fece980bbb1fddcc1dc3ba1908f99
MK <148148795+mkiyah-b@users.noreply.github.com>
2026-09-10 23:32:57 -0400

    Update afile.js

--- afile.js
+++ afile.js
@@ -32,7 +32,7 @@ else {
         // console.log(randomHeight);
         firefly.style.top = randomHeight + "px";
         
-        let randomWidth = (Math.random() * 650) + jarPos.x + 1000; 
+        let randomWidth = (Math.random() * 650) + jarPos.x + 650; 
         // console.log(randomWidth);
         firefly.style.left = randomWidth + "px";
         
